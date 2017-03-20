c14bd0652166a32882128da3d1829246355f3178
holmesworcester <hwilson@gmail.com>
2017-03-20 01:22:39 -0400

    Trying deploy with now

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 // todo:
+// figure out why github deploy doesn't work!! (possibly the problem they mention??)
+// trying "now"
 // make ship movement more fluid/natural.
 // add a missle and a way to shoot it.
 // add an adversary
@@ -15,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Try the arrow keys. The ship <em>moves</em>!</h1>
        <ShipContainer />
       </div>
     );

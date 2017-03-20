73d0c6e7a233b1f6479273b96149c4b16b06829f
holmesworcester <hwilson@gmail.com>
2017-03-20 00:02:37 -0400

    Added some notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
+// todo:
+// make ship movement more fluid/natural.
+// add a missle and a way to shoot it.
+// add an adversary
+// Should containers and components be in the same directory or different ones?
+// It seems like in the context of a game it's fine as is. 
+// Or I could make the containers a bit more complex and just have them place SVGs on the screen.
+
 import React, { Component } from 'react';
 import './App.css';
 import ShipContainer from './Ship/ShipContainer.js';
-
-// @keydown looks like decorators don't work.
 
 class App extends Component {
 
--- src/components/Ship/ShipContainer.js
+++ src/components/Ship/ShipContainer.js
@@ -1,3 +1,8 @@
+// todo: 
+// figure out how to do stuff on clock tick
+// establish the ship's velocity and just change it.
+// or figure out how to tell when keys are being held down (might be harder)
+
 import React, { Component } from 'react';
 import Ship from './Ship.js';
 import ReactKeymaster from 'react-keymaster';
@@ -33,6 +38,8 @@ class ShipContainer extends Component {
     });
   }
   */
+
+  // I don't totally understand how ReactKeymaster is being called here. I guess it's a component that just updates state. I wish I could have it listen to all keys and tell me what was pressed.
 
   render() {
     return (
